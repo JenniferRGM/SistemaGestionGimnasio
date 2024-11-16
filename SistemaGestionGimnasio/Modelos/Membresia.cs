a7d5019bff799f5bdb8cc8ad64a368b3a7fe748a
JENNIFER REBECA GARCIA MONCADA <jrgarcia@edu.upolitecnica.cr>
2024-11-15 22:12:56 -0600

    Se crea la clase membresia

---
+++ SistemaGestionGimnasio/Modelos/Membresia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionGimnasio.Modelos
+{
+    internal class Membresia
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+
+        public Membresia(DateTime fechaInicio, DateTime fechaVencimiento)
+        {
+            FechaInicio = fechaInicio;
+            FechaVencimiento = fechaVencimiento;
+        }
+
+        public int DiasRestantes()
+        {
+            return (FechaVencimiento - DateTime.Now).Days;
+        }
+
+        public bool EstaPorVencer()
+        {
+            return DiasRestantes() <= 5;
+        }
+    }
+}
