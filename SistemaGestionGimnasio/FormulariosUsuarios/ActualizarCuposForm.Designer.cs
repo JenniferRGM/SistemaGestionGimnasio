1b869ab365e189de9c4b6e47032ac6d30e291bda
JENNIFER REBECA GARCIA MONCADA <jrgarcia@edu.upolitecnica.cr>
2024-11-19 20:52:12 -0600

    Se agrega el codigo para Actualizar los cupos

--- SistemaGestionGimnasio/FormulariosUsuarios/ActualizarCuposForm.Designer.cs
+++ SistemaGestionGimnasio/FormulariosUsuarios/ActualizarCuposForm.Designer.cs
@@ -41,19 +41,21 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(237, 13);
+            this.label1.Location = new System.Drawing.Point(320, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 30);
+            this.label1.Size = new System.Drawing.Size(314, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Actualización de Clases";
+            this.label1.Text = "Actualización de Cupos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 80);
+            this.label2.Location = new System.Drawing.Point(36, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.Size = new System.Drawing.Size(80, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "Clase:";
             // 
@@ -61,51 +63,60 @@
             // 
             this.CmbClases.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmbClases.FormattingEnabled = true;
-            this.CmbClases.Location = new System.Drawing.Point(111, 83);
+            this.CmbClases.Location = new System.Drawing.Point(148, 102);
+            this.CmbClases.Margin = new System.Windows.Forms.Padding(4);
             this.CmbClases.Name = "CmbClases";
-            this.CmbClases.Size = new System.Drawing.Size(172, 23);
+            this.CmbClases.Size = new System.Drawing.Size(228, 28);
             this.CmbClases.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(32, 151);
+            this.label3.Location = new System.Drawing.Point(43, 186);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.Size = new System.Drawing.Size(81, 32);
             this.label3.TabIndex = 3;
             this.label3.Text = "Cupo:";
             // 
             // NumNuevoCupo
             // 
-            this.NumNuevoCupo.Location = new System.Drawing.Point(111, 155);
+            this.NumNuevoCupo.Location = new System.Drawing.Point(148, 191);
+            this.NumNuevoCupo.Margin = new System.Windows.Forms.Padding(4);
             this.NumNuevoCupo.Name = "NumNuevoCupo";
-            this.NumNuevoCupo.Size = new System.Drawing.Size(120, 20);
+            this.NumNuevoCupo.Size = new System.Drawing.Size(160, 22);
             this.NumNuevoCupo.TabIndex = 4;
             // 
             // BtnActualizarCupo
             // 
+            this.BtnActualizarCupo.BackColor = System.Drawing.Color.LightSkyBlue;
             this.BtnActualizarCupo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnActualizarCupo.Location = new System.Drawing.Point(165, 219);
+            this.BtnActualizarCupo.Location = new System.Drawing.Point(287, 279);
+            this.BtnActualizarCupo.Margin = new System.Windows.Forms.Padding(4);
             this.BtnActualizarCupo.Name = "BtnActualizarCupo";
-            this.BtnActualizarCupo.Size = new System.Drawing.Size(123, 57);
+            this.BtnActualizarCupo.Size = new System.Drawing.Size(138, 70);
             this.BtnActualizarCupo.TabIndex = 5;
             this.BtnActualizarCupo.Text = "Actualizar";
-            this.BtnActualizarCupo.UseVisualStyleBackColor = true;
+            this.BtnActualizarCupo.UseVisualStyleBackColor = false;
+            this.BtnActualizarCupo.Click += new System.EventHandler(this.BtnActualizarCupo_Click);
             // 
             // ActualizarCuposForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.BtnActualizarCupo);
             this.Controls.Add(this.NumNuevoCupo);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.CmbClases);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ActualizarCuposForm";
             this.Text = "ActualizarCuposForm";
+            this.Load += new System.EventHandler(this.ActualizarCuposForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.NumNuevoCupo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- SistemaGestionGimnasio/FormulariosUsuarios/ActualizarCuposForm.cs
+++ SistemaGestionGimnasio/FormulariosUsuarios/ActualizarCuposForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,97 @@ namespace SistemaGestionGimnasio.FormulariosUsuarios
         {
             InitializeComponent();
         }
+
+        private void ActualizarCuposForm_Load(object sender, EventArgs e)
+        {
+            CargarClasesEnComboBox();
+        }
+
+        private void CargarClasesEnComboBox()
+        {
+            string rutaArchivo = "clases.csv";
+
+            if (!File.Exists(rutaArchivo))
+            {
+                MessageBox.Show("No se encontraron clases registradas.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            CmbClases.Items.Clear();
+
+            using (StreamReader lector = new StreamReader(rutaArchivo))
+            {
+                string linea;
+                while ((linea = lector.ReadLine()) != null)
+                {
+                    string[] datos = linea.Split(',');
+
+                    if (datos.Length >= 1)
+                    {
+                        CmbClases.Items.Add(datos[0]);
+                    }
+                }
+            }
+        }
+
+        private void BtnActualizarCupo_Click(object sender, EventArgs e)
+        {
+            string claseSeleccionada = CmbClases.SelectedItem?.ToString();
+            int nuevoCupo = (int)NumNuevoCupo.Value;
+
+            if (string.IsNullOrEmpty(claseSeleccionada))
+            {
+                MessageBox.Show("Por favor, selecciona una clase.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ActualizarCupoEnArchivo(claseSeleccionada, nuevoCupo);
+        }
+
+        private static void ActualizarCupoEnArchivo(string clase, int nuevoCupo)
+        {
+            string rutaArchivo = "actividades.csv";
+            string rutaTemporal = "actividades_temp.csv";
+
+            if (!File.Exists(rutaArchivo))
+            {
+                MessageBox.Show("No se encontró el archivo de clases.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (StreamReader lector = new StreamReader(rutaArchivo))
+            using (StreamWriter escritor = new StreamWriter(rutaTemporal))
+            {
+                string linea;
+                bool actualizado = false;
+
+                while ((linea = lector.ReadLine()) != null)
+                {
+                    string[] datos = linea.Split(',');
+
+                    if (datos.Length >= 3 && datos[0] == clase)
+                    {
+                        escritor.WriteLine($"{datos[0]},{datos[1]},{nuevoCupo}");
+                        actualizado = true;
+                    }
+
+                    else
+                    {
+                        escritor.WriteLine(linea);
+                    }
+                }
+
+                if (!actualizado)
+                {
+                    MessageBox.Show("La clase seleccionada no se encontró.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            File.Delete(rutaArchivo);
+            File.Move(rutaTemporal, rutaArchivo);
+
+            MessageBox.Show("Cupo actualizado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
+
