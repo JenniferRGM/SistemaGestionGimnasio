ecf80e006578516e1242432cc85d597173ec79cb
YOSTIN VINICIO LUNA SANCHEZ <yluna@edu.upolitecnica.cr>
2024-11-19 21:34:10 -0600

    Se crea el formulario Consultar Reservas Clientes

--- SistemaGestionGimnasio/FormulariosUsuarios/ActualizarCuposForm.Designer.cs
+++ SistemaGestionGimnasio/FormulariosUsuarios/ActualizarCuposForm.Designer.cs
@@ -41,10 +41,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(320, 19);
+            this.label1.Location = new System.Drawing.Point(360, 24);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(314, 37);
+            this.label1.Size = new System.Drawing.Size(367, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Actualización de Cupos";
             // 
@@ -52,10 +52,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 98);
+            this.label2.Location = new System.Drawing.Point(40, 122);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 32);
+            this.label2.Size = new System.Drawing.Size(96, 40);
             this.label2.TabIndex = 1;
             this.label2.Text = "Clase:";
             // 
@@ -63,39 +63,39 @@
             // 
             this.CmbClases.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmbClases.FormattingEnabled = true;
-            this.CmbClases.Location = new System.Drawing.Point(148, 102);
-            this.CmbClases.Margin = new System.Windows.Forms.Padding(4);
+            this.CmbClases.Location = new System.Drawing.Point(166, 128);
+            this.CmbClases.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CmbClases.Name = "CmbClases";
-            this.CmbClases.Size = new System.Drawing.Size(228, 28);
+            this.CmbClases.Size = new System.Drawing.Size(256, 33);
             this.CmbClases.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(43, 186);
+            this.label3.Location = new System.Drawing.Point(48, 232);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 32);
+            this.label3.Size = new System.Drawing.Size(97, 40);
             this.label3.TabIndex = 3;
             this.label3.Text = "Cupo:";
             // 
             // NumNuevoCupo
             // 
-            this.NumNuevoCupo.Location = new System.Drawing.Point(148, 191);
-            this.NumNuevoCupo.Margin = new System.Windows.Forms.Padding(4);
+            this.NumNuevoCupo.Location = new System.Drawing.Point(166, 239);
+            this.NumNuevoCupo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.NumNuevoCupo.Name = "NumNuevoCupo";
-            this.NumNuevoCupo.Size = new System.Drawing.Size(160, 22);
+            this.NumNuevoCupo.Size = new System.Drawing.Size(180, 26);
             this.NumNuevoCupo.TabIndex = 4;
             // 
             // BtnActualizarCupo
             // 
             this.BtnActualizarCupo.BackColor = System.Drawing.Color.LightSkyBlue;
             this.BtnActualizarCupo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnActualizarCupo.Location = new System.Drawing.Point(287, 279);
-            this.BtnActualizarCupo.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnActualizarCupo.Location = new System.Drawing.Point(323, 349);
+            this.BtnActualizarCupo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnActualizarCupo.Name = "BtnActualizarCupo";
-            this.BtnActualizarCupo.Size = new System.Drawing.Size(138, 70);
+            this.BtnActualizarCupo.Size = new System.Drawing.Size(181, 88);
             this.BtnActualizarCupo.TabIndex = 5;
             this.BtnActualizarCupo.Text = "Actualizar";
             this.BtnActualizarCupo.UseVisualStyleBackColor = false;
@@ -103,17 +103,17 @@
             // 
             // ActualizarCuposForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.BtnActualizarCupo);
             this.Controls.Add(this.NumNuevoCupo);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.CmbClases);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ActualizarCuposForm";
             this.Text = "ActualizarCuposForm";
             this.Load += new System.EventHandler(this.ActualizarCuposForm_Load);
---
+++ SistemaGestionGimnasio/FormulariosUsuarios/ConsultarReservasForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SistemaGestionGimnasio.FormulariosUsuarios
+{
+    partial class ConsultarReservasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnConsultarReservas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consultar Reservas Clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(62, 231);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(366, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Clase";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Fecha";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // BtnConsultarReservas
+            // 
+            this.BtnConsultarReservas.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultarReservas.Location = new System.Drawing.Point(260, 436);
+            this.BtnConsultarReservas.Name = "BtnConsultarReservas";
+            this.BtnConsultarReservas.Size = new System.Drawing.Size(178, 57);
+            this.BtnConsultarReservas.TabIndex = 4;
+            this.BtnConsultarReservas.Text = "Consultar";
+            this.BtnConsultarReservas.UseVisualStyleBackColor = true;
+            // 
+            // ConsultarReservasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 564);
+            this.Controls.Add(this.BtnConsultarReservas);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ConsultarReservasForm";
+            this.Text = "ConsultarReservasForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button BtnConsultarReservas;
+    }
+}
