330e871dde1666410d421edbe7d4470564212257
YOSTIN VINICIO LUNA SANCHEZ <yluna@edu.upolitecnica.cr>
2024-12-14 17:12:23 -0600

    Se crean clases de cliente, clases populares, entrenador, combobox

---
+++ ProyectoBlazor/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBlazor.Modelos
+{
+    internal class Cliente : Usuario
+    {
+        public Cliente(int id, string nombre, string correo, string nombreUsuario, string contraseña)
+        : base(id, nombre, correo, "Cliente", contraseña, nombreUsuario)
+        {
+        }
+
+    }
+}
---
+++ ProyectoBlazor/Models/Entrenador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBlazor.Modelos
+{
+    public class Entrenador : Usuario
+    {
+        public string PuntosFuertes { get; set; }
+        public List<string> Horarios { get; set; }
+
+        public Entrenador(int id, string nombre, string correo, string nombreUsuario, string contraseña, string puntosFuertes)
+        : base(id, nombre, correo, "Entrenador", contraseña, nombreUsuario)
+        {
+            PuntosFuertes = puntosFuertes;
+            Horarios = new List<string>();
+        }
+    }
+}
